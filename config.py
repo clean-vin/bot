f9cea511bac3df432cf29d6d07c37d8c07699954
RM <cars.martsin@gmail.com>
2025-05-07 13:40:40 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # config.py
 import os
-BOT_TOKEN = os.getenv("7763416844:AAFm_pi0I9FCT2kyB5jxgYLajvAsCugcFxA")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 MANAGER_CHAT_ID = os.getenv("203508229")
-
 PAYPAL_LINK = os.getenv("https://www.paypal.com/ncp/payment/SJJ32EJZ66F6C")
 CRYPTO_WALLET = os.getenv("0x1c9945cee7588600da68fd394ae7783adeddbd18")
